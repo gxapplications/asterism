829adb20b4db28ce794d7d73c019fa0c87c3bf3c
gxapplications <xavier.gouley@gmail.com>
2017-07-12 01:28:51 +0200

    hot reload module
    react hot reloader

--- lib/browser/builder/bootstrap.jsx
+++ lib/browser/builder/bootstrap.jsx
@@ -1,8 +1,20 @@
 'use strict'
 
 import React from 'react'
-import { render } from 'react-dom'
+import { render as renderIntoDom } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
-import { MainComponent } from '../main-component'
+import MainComponent from '../main-component'
 
-render(<MainComponent />, document.getElementById('app'))
+const render = (Component) => {
+  renderIntoDom(<AppContainer><Component /></AppContainer>, document.getElementById('app'))
+}
+
+render(MainComponent)
+
+if (module.hot) {
+  module.hot.accept('../main-component', () => {
+    const NextMainComponent = require('../main-component').MainComponent
+    render(NextMainComponent)
+  })
+}
