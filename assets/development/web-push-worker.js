53757263e04dced98a62777674bc09c54f3e020c
Xavier Gouley <xavier.gouley@gmail.com>
2020-03-24 18:04:47 +0100

    more success trying to have working PWA

--- assets/development/web-push-worker.js
+++ assets/development/web-push-worker.js
@@ -1,10 +1,26 @@
-/* global self, clients, caches, fetch */
+/* global self, clients, caches, fetch, URL */
 
-const CACHE_VERSION = '1.2.9'
-const FILES_TO_CACHE = ['/offline.html', '/']
+const CACHE_VERSION = '1.2.9.2'
+const FILES_TO_CACHE = ['', '/',
+  '/web-push-worker.js',
+  '/manifest.json',
+  '/offline.html',
+  '/jquery/jquery.min.js',
+  '/node-forge/forge.min.js',
+  '/assets/patternlock.min.js',
+  '/assets/patternlock.min.css',
+  '/assets/material-icons.css',
+  '/materialize-css/css/materialize.min.css',
+  '/materialize-css-extras/noUiSlider/nouislider.css',
+  '/materialize-css/js/materialize.min.js',
+  '/assets/jquery.initialize.min.j',
+  '/build/common.js',
+  '/build/plugins.js',
+  '/build/bundle.js'
+]
 
 self.addEventListener('install', ev => {
-  console.log(`Installing new web-push worker version...`)
+  console.log(`[ServiceWorker] Installing new web-push worker version...`)
 
   ev.waitUntil(
     caches.open(CACHE_VERSION).then((cache) => {
@@ -17,6 +33,8 @@ self.addEventListener('install', ev => {
 })
 
 self.addEventListener('activate', ev => {
+  console.log(`[ServiceWorker] Activating service worker...`)
+
   ev.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
@@ -35,6 +53,10 @@ self.addEventListener('fetch', ev => {
   ev.respondWith(
     // Try the cache
     caches.match(ev.request).then(response => {
+      const url = new URL(ev.request.url)
+      if (url.pathname === '/') {
+        return fetch(ev.request)
+      }
       return response || fetch(ev.request)
     }).catch(() => {
       return caches.match('/offline.html')
--- assets/production/web-push-worker.js
+++ assets/production/web-push-worker.js
@@ -1,7 +1,67 @@
-/* global self, clients */
-self.addEventListener('install', () => {
-  console.log(`Installing new web-push worker version...`)
+/* global self, clients, caches, fetch, URL */
+
+const CACHE_VERSION = '1.2.9.2'
+const FILES_TO_CACHE = ['', '/',
+  '/web-push-worker.js',
+  '/manifest.json',
+  '/offline.html',
+  '/jquery/jquery.min.js',
+  '/node-forge/forge.min.js',
+  '/assets/patternlock.min.js',
+  '/assets/patternlock.min.css',
+  '/assets/material-icons.css',
+  '/materialize-css/css/materialize.min.css',
+  '/materialize-css-extras/noUiSlider/nouislider.css',
+  '/materialize-css/js/materialize.min.js',
+  '/assets/jquery.initialize.min.j',
+  '/build/common.js',
+  '/build/plugins.js',
+  '/build/bundle.js'
+]
+
+self.addEventListener('install', ev => {
+  console.log(`[ServiceWorker] Installing new web-push worker version...`)
+
+  ev.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page')
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+
   return self.skipWaiting()
+})
+
+self.addEventListener('activate', ev => {
+  console.log(`[ServiceWorker] Activating service worker...`)
+
+  ev.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_VERSION) {
+          console.log('[ServiceWorker] Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+
+  return self.clients.claim()
+})
+
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    // Try the cache
+    caches.match(ev.request).then(response => {
+      const url = new URL(ev.request.url)
+      if (url.pathname === '/') {
+        return fetch(ev.request)
+      }
+      return response || fetch(ev.request)
+    }).catch(() => {
+      return caches.match('/offline.html')
+    })
+  )
 })
 
 self.addEventListener('push', ev => {
